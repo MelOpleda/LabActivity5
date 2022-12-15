cef60acb6acb3dc97530a017e37a23aa2c928dda
Melchiah <melchiahopleda1999@gmail.com>
2022-12-15 18:52:28 +0800

    Rename LeapYear components

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,9 +1,44 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.JOptionPane;
 
-public class LeapYearGUI {
-    private JPanel panel1;
-    private JTextField textField1;
-    private JButton checkButton;
+public class LeapYearGUI extends JFrame implements ActionListener{
+    private JPanel pnlMain;
+    private JTextField tfYear;
+    private JButton btnCheckYear;
+
+    public static void main(String[] args) {
+        LeapYearGUI app = new LeapYearGUI();
+        app.setTitle("Leap Year Checker");
+        app.setContentPane(app.pnlMain);
+        app.setSize(500, 250);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+        app.setLocationRelativeTo(null);
+        app.btnCheckYear.addActionListener(new ActionListener() {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+            int year;
+            try {
+                year = Integer.parseInt(app.tfYear.getText());
+            } catch (NumberFormatException numberFormatException) {
+                return;
+            }
+            if(((year %4 == 0)&&(year %100 != 0))||year %400 == 0)
+                JOptionPane.showMessageDialog(null,
+                        "Leap year", "Message", JOptionPane.INFORMATION_MESSAGE);
+            else
+                JOptionPane.showMessageDialog(null,
+                        "Not a leap year", "Message", JOptionPane.INFORMATION_MESSAGE);
+    }
+        });
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
+
