20c7f79c8ecaab91bbde3dc1f53192ca7d546e67
Melchiah <melchiahopleda1999@gmail.com>
2022-12-16 16:39:14 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,8 +1,9 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.*;
 
-public class LeapYearGUI extends JFrame{
+public class LeapYearGUI extends JFrame implements ActionListener {
     private JPanel pnlMain;
     private JTextField tfYear;
     private JButton btnCheckYear;
@@ -16,7 +17,27 @@ public class LeapYearGUI extends JFrame{
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
         app.setLocationRelativeTo(null);
+
+        app.btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int year;
+                try {
+                    year = Integer.parseInt(app.tfYear.getText());
+                } catch (NumberFormatException numberFormatException) {
+                    return;
+                }
+                if(((year %4 == 0)&&(year %100 != 0))||year %400 == 0)
+                    System.out.println("Leap Year");
+                else
+                    System.out.println("Not a Leap Year");
+            }
+        });
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
 
