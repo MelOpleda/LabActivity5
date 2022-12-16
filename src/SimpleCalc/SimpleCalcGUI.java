bc58a3711881b8b2589af541f23342b2c9e251ea
Melchiah <melchiahopleda1999@gmail.com>
2022-12-16 17:00:08 +0800

    Add SimpleCalc main function

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,8 +1,9 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.*;
 
-public class SimpleCalcGUI{
+public class SimpleCalcGUI extends JFrame {
     private JPanel pnlMain;
     private JTextField tfNumber1;
     private JComboBox cbOperations;
@@ -10,4 +11,26 @@ public class SimpleCalcGUI{
     private JTextField tfNumber2;
     private JTextField lblResult;
 
+
+    public static void main(String[] args) {
+        SimpleCalcGUI app2 = new SimpleCalcGUI();
+        app2.setTitle("Sample Calculator");
+        app2.setContentPane(app2.pnlMain);
+        app2.setSize(500, 250);
+        app2.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app2.setVisible(true);
+        app2.setLocationRelativeTo(null);
+        app2.lblResult.setEditable(false);
+
+        app2.tfNumber1.setHorizontalAlignment(JLabel.CENTER);
+        app2.tfNumber2.setHorizontalAlignment(JLabel.CENTER);
+        app2.lblResult.setHorizontalAlignment(JLabel.CENTER);
+
+        app2.cbOperations.addItem("+");
+        app2.cbOperations.addItem("-");
+        app2.cbOperations.addItem("*");
+        app2.cbOperations.addItem("/");
+
+
+    }
 }
