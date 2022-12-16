ac9b607d8ac14ca9fbf6c2a5dc0ac5cc0786e5e0
Melchiah <melchiahopleda1999@gmail.com>
2022-12-16 16:31:41 +0800

    Rename LeapYear components

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,10 +1,81 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.*;
 
-public class FoodOrderGUI {
+public class FoodOrderGUI extends JFrame implements ActionListener {
     private JPanel panel1;
-    private JCheckBox pizzaCheckBox;
-    private JRadioButton noneRadioButton;
-    private JButton orderButton;
+    private JCheckBox cPizza;
+    private JRadioButton rbNone;
+    private JButton btnOrder;
+    private JCheckBox cBurger;
+    private JCheckBox cFries;
+    private JCheckBox cSoftDrinks;
+    private JCheckBox cTea;
+    private JCheckBox cSundae;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
+
+
+    public static void main(String[] args) {
+        FoodOrderGUI app3 = new FoodOrderGUI();
+        app3.setTitle("Food Ordering System");
+        app3.setContentPane(app3.panel1);
+        app3.setSize(600, 600);
+        app3.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app3.setVisible(true);
+        app3.setLocationRelativeTo(null);
+
+
+
+        app3.btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                double total = 0.00;
+
+                if(app3.cPizza.isSelected()){
+                    total+=100.00;
+                }
+                if(app3.cBurger.isSelected()){
+                    total+=80.00;
+                }
+                if(app3.cFries.isSelected()){
+                    total+=65.00;
+                }
+                if(app3.cSoftDrinks.isSelected()){
+                    total+=55.00;
+                }
+                if(app3.cTea.isSelected()){
+                    total+=50.00;
+                }
+                if(app3.cSundae.isSelected()){
+                    total+=40.00;
+                }
+
+                if(app3.rb5.isSelected()){
+                    double discount = 100 - 5;
+                    total*=discount/100;
+                }
+                else if(app3.rb10.isSelected()){
+                    double discount = 100 - 10;
+                    total*=discount/100;
+                }
+                else if(app3.rb15.isSelected()){
+                    double discount = 100 - 15;
+                    total*=discount/100;
+                }
+                    JOptionPane.showMessageDialog(null,String.format("The total price is Php %.2f", total));
+            }
+        });
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
+
+
+
